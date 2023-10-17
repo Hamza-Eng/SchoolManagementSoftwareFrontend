85f5e727131422d2d1e62ea0c040ecfa8b08aff2
Hamza Madani <hamzamadani217@gmail.com>
2023-10-17 23:40:48 +0100

    change name to :  school management system

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Ecolde
+# school management system
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 15.1.0.
 
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ecolde'`, () => {
+  it(`should have as title 'school management system'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ecolde');
+    expect(app.title).toEqual('school management system');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('ecolde app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('school management system app is running!');
   });
 });
