18264ab53ac9f096bf5150c0c2266578b9f2fc6c
Hamza Madani <hamzamadani217@gmail.com>
2023-10-17 23:35:54 +0100

    add table componenet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {MatTableModule} from '@angular/material/table';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -11,13 +12,19 @@ import {MatToolbarModule} from "@angular/material/toolbar";
 import { MenubarComponent } from './menubar/menubar.component';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatListModule} from '@angular/material/list';
+import { TableComponent } from './table/table.component';
+import { MatSortModule } from '@angular/material/sort';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenubarComponent
+    MenubarComponent,
+    TableComponent
+    
   ],
   imports: [
+    MatSortModule,
+    MatTableModule,
     MatListModule,
     MatSidenavModule,
     BrowserModule,
