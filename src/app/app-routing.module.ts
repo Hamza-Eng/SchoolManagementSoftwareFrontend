3a542c4f6805b8149066528c3a0a3a805fe3ccdb
Hamza Madani <hamzamadani217@gmail.com>
2023-10-27 20:22:06 +0100

    add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { MenubarComponent } from './utility/menubar/menubar.component';
 
 const routes: Routes = [
-  {path:"home",component:MenubarComponent}
+  {path:"home",component:MenubarComponent},
+  {path:"center",component:MenubarComponent},
+  {path:"cycle",component:MenubarComponent},
+  {path:"student",component:MenubarComponent},
+  {path:"establishment",component:MenubarComponent},
+  {path:"branch",component:MenubarComponent},
+  {path:"**",redirectTo : "home"},
+  
 ];
 
 @NgModule({
