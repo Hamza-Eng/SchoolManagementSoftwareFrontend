588fee9c6f96260958e50f5e584748d02dcb50e7
Hamza Madani <hamzamadani217@gmail.com>
2023-11-03 19:30:46 +0000

    add  table componenet in each of establishment...

--- src/app/establishment/component/establishment.component.ts
+++ src/app/establishment/component/establishment.component.ts
@@ -1,14 +1,33 @@
-import { Component } from '@angular/core';
-import { MatTableDataSource } from '@angular/material/table';
+import { Component, OnInit } from '@angular/core';
 import { PeriodicElement } from 'src/app/utility/table/table.component';
+import {MatTableDataSource} from '@angular/material/table';
 
+const ELEMENT_DATA: any[] = [
+  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H',hamza:'hamza'},
+  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He',hamza:'hamza'},
+  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li',hamza:'hamza'},
+  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be',hamza:'hamza'},
+  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B',hamza:'hamza'},
+  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C',hamza:'hamza'},
+  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N',hamza:'hamza'},
+  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O',hamza:'hamza'},
+  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F',hamza:'hamza'},
+  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne',hamza:'hamza'},
+];
 @Component({
   selector: 'app-establishment',
   templateUrl: './establishment.component.html',
   styleUrls: ['./establishment.component.css']
 })
-export class EstablishmentComponent {
+export class EstablishmentComponent implements OnInit{
   constructor(){
 
   }
+  dataSource :any;
+  displayedColumns: any;
+  ngOnInit(): void {
+   this.dataSource = new MatTableDataSource(ELEMENT_DATA);
+   this.displayedColumns= ['position', 'name', 'weight', 'symbol','hamza']
+  }
+ 
 }
--- src/app/utility/table/table.component.ts
+++ src/app/utility/table/table.component.ts
@@ -1,5 +1,5 @@
 import {LiveAnnouncer} from '@angular/cdk/a11y';
-import {AfterViewInit,OnInit, Component, ViewChild} from '@angular/core';
+import {AfterViewInit,OnInit, Component, ViewChild, Input} from '@angular/core';
 import {MatSort, Sort} from '@angular/material/sort';
 import {MatTableDataSource} from '@angular/material/table';
 import { EstablishmentService } from 'src/app/establishment/establishment.service';
@@ -11,18 +11,18 @@ export interface PeriodicElement {
   weight: number;
   symbol: string;
 }
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-];
+// const ELEMENT_DATA: PeriodicElement[] = [
+//   {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
+//   {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
+//   {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
+//   {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
+//   {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
+//   {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
+//   {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
+//   {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
+//   {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
+//   {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+// ];
 /**
  * @title Table with sorting
  */
@@ -34,8 +34,9 @@ const ELEMENT_DATA: PeriodicElement[] = [
 })
 export class TableComponent implements AfterViewInit , OnInit{
 
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-  dataSource = new MatTableDataSource(ELEMENT_DATA);
+  @Input() displayedColumns:any;
+  // @Input()  data!:any[];
+  @Input() dataSource:any;
 
   constructor(private _liveAnnouncer: LiveAnnouncer,private etablisment:EstablishmentService) {}
   ngOnInit(): void {
