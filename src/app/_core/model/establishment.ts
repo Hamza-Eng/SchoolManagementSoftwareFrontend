ddc0ac356fb56e615f08f62f5dbb6964306210cd
Hamza Madani <hamzamadani217@gmail.com>
2023-11-14 21:41:17 +0100

    add  etsblisment moul

---
+++ src/app/_core/model/establishment.ts
@@ -0,0 +1,36 @@
+export class establishment{
+    
+	    societe !:String;
+
+	    adresse !:String;
+
+	    ville !:String;
+
+	    email !:String;
+
+	    descc !:String;
+
+	    web !:String;
+
+	    ice !:String;
+
+	    rc !:String;
+
+	    patente !:String;
+
+	    cnss !:String;
+
+	    ifs !:String;
+
+	    tel !:String;
+
+	    portable !:String;
+
+	    banque !:String;
+
+	    agence !:String;
+
+	    rib !:String;
+
+	    image !:String;
+}
